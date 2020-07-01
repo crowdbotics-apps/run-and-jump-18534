23842954136e0543b782fa2988e5d4a2cbf6181a
Crowdbotics <team@crowdbotics.com>
2020-07-01 16:40:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/run_and_jump_18534/MainActivity.java
@@ -0,0 +1,15 @@
+package com.run_and_jump_18534;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "run_and_jump_18534";
+    }
+}
